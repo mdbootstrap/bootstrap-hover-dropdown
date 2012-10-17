a6a139ab433b0ca15e334e8ecf682431405f9f07
Cameron Spear <cameron@matchbooklab.com>
2012-10-17 10:09:29 -0700

    change how options work, and make it work with data-attributes

--- twitter-bootstrap-hover-dropdown.js
+++ twitter-bootstrap-hover-dropdown.js
@@ -11,27 +11,40 @@
  * http://cameronspear.com/blog/twitter-bootstrap-dropdown-on-hover-plugin/
  */
 ;(function($, window, undefined) {
+    // outside the scipe of the jQuery plugin to keep track of all dropdowns
+    var $allDropdowns = $();
+
     // if instantlyCloseOthers is true, then it will instantly shut other nav items when a new one is hovered over
-    $.fn.dropdownHover = function(delay, instantlyCloseOthers) {
-        if(typeof delay === 'boolean') {
-            instantlyCloseOthers = delay;
-            delay = 500;
-        }
-        else if(!delay) delay = 500;
+    $.fn.dropdownHover = function(options) {
 
         // the element we really care about is the dropdown-toggle's parent
-        var $these = this.parent();
+        $allDropdowns = $allDropdowns.add(this.parent());
+
         return this.each(function() {
-            var timeout, $this = $(this).parent();
+            var $this = $(this).parent(),
+                defaults = {
+                    delay: 500,
+                    instantlyCloseOthers: true
+                },
+                data = {
+                    delay: $(this).data('delay'),
+                    instantlyCloseOthers: $(this).data('close-others')
+                },
+                options = $.extend(true, {}, defaults, options, data),
+                timeout;
+
             $this.hover(function() {
                 window.clearTimeout(timeout);
-                if(instantlyCloseOthers === true) $these.removeClass('open');
+                console.log(options.instantlyCloseOthers, typeof options.instantlyCloseOthers);
+                if(options.instantlyCloseOthers === true) $allDropdowns.removeClass('open');
                 $(this).addClass('open');
             }, function() {
                 timeout = window.setTimeout(function() {
                     $this.removeClass('open');
-                }, delay);
+                }, options.delay);
             });
         });
     };
+
+    $('[data-hover="dropdown"]').dropdownHover();
 })(jQuery, this);
