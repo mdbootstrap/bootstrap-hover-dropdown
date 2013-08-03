0962a027d536cab956860ae92acc180c8c6c25ac
Cameron Spear <cameronspear@gmail.com>
2013-08-02 22:05:07 -0700

    Remove code surrounding submenus and mobile stuff (no longer applies in Bootstrap 3).

--- twitter-bootstrap-hover-dropdown.js
+++ twitter-bootstrap-hover-dropdown.js
@@ -44,31 +44,24 @@
                     return true;
                 }
 
-                if(shouldHover) {
-                    if(settings.instantlyCloseOthers === true)
-                        $allDropdowns.removeClass('open');
+                if(settings.instantlyCloseOthers === true)
+                    $allDropdowns.removeClass('open');
 
-                    window.clearTimeout(timeout);
-                    $parent.addClass('open');
-                }
+                window.clearTimeout(timeout);
+                $parent.addClass('open');
             }, function() {
-                if(shouldHover) {
-                    timeout = window.setTimeout(function() {
-                        $parent.removeClass('open');
-                    }, settings.delay);
-                }
-
+                timeout = window.setTimeout(function() {
+                    $parent.removeClass('open');
+                }, settings.delay);
             });
 
             // this helps with button groups!
             $this.hover(function() {
-                if(shouldHover) {
-                    if(settings.instantlyCloseOthers === true)
-                        $allDropdowns.removeClass('open');
+                if(settings.instantlyCloseOthers === true)
+                    $allDropdowns.removeClass('open');
 
-                    window.clearTimeout(timeout);
-                    $parent.addClass('open');
-                }
+                window.clearTimeout(timeout);
+                $parent.addClass('open');
             });
 
             // handle submenus
@@ -76,60 +69,22 @@
                 var $this = $(this);
                 var subTimeout;
                 $this.hover(function() {
-                    if(shouldHover) {
-                        window.clearTimeout(subTimeout);
-                        $this.children('.dropdown-menu').show();
-                        // always close submenu siblings instantly
-                        $this.siblings().children('.dropdown-menu').hide();
-                    }
+                    window.clearTimeout(subTimeout);
+                    $this.children('.dropdown-menu').show();
+                    // always close submenu siblings instantly
+                    $this.siblings().children('.dropdown-menu').hide();
                 }, function() {
                     var $submenu = $this.children('.dropdown-menu');
-                    if(shouldHover) {
-                        subTimeout = window.setTimeout(function() {
-                            $submenu.hide();
-                        }, settings.delay);
-                    } else {
-                        // emulate Twitter Bootstrap's default behavior
+                    subTimeout = window.setTimeout(function() {
                         $submenu.hide();
-                    }
+                    }, settings.delay);
                 });
             });
         });
     };
 
-    // helper variables to guess if they are using a mouse
-    var shouldHover = false,
-        mouse_info = {
-            hits: 0,
-            x: null,
-            y: null
-        };
     $(document).ready(function() {
         // apply dropdownHover to all elements with the data-hover="dropdown" attribute
         $('[data-hover="dropdown"]').dropdownHover();
-
-        // if the mouse movements are "smooth" or there are more than 20, they probably have a real mouse
-        $(document).mousemove(function(e){
-            mouse_info.hits++;
-            if (mouse_info.hits > 20 || (Math.abs(e.pageX - mouse_info.x) + Math.abs(e.pageY - mouse_info.y)) < 4) {
-                $(this).unbind(e);
-                shouldHover = true;
-            }
-            else {
-                mouse_info.x = e.pageX;
-                mouse_info.y = e.pageY;
-            }
-        });
     });
-
-    // for the submenu to close on delay, we need to override Bootstrap's CSS in this case
-    var css = '.dropdown-submenu:hover>.dropdown-menu{display:none}';
-    var style = document.createElement('style');
-    style.type = 'text/css';
-    if (style.styleSheet) {
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
-    $('head')[0].appendChild(style);
 })(jQuery, this);
