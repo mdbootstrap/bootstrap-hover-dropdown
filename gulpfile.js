eb0ab31f9e187973beaf94f356797e9d5ae240a9
Cameron Spear <cameronspear@gmail.com>
2015-02-07 13:12:57 -0800

    Add a gulp file to perform minifications (and help manage releases)

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp   = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+ 
+gulp.task('default', function() {
+    gulp.src('bootstrap-hover-dropdown.js')
+        // minifiy preserving preserved comments
+        .pipe(uglify({
+            preserveComments: 'some'
+        }))
+
+        // rename to .min.
+        .pipe(rename('bootstrap-hover-dropdown.min.js'))
+        
+        .pipe(gulp.dest('.'));
+});
+
+
+var bump = require('gulp-bump');
+var filter = require('gulp-filter');
+var git = require('gulp-git');
+var tagVersion = require('gulp-tag-version');
+ 
+/**
+ * Bumping version number and tagging the repository with it.
+ * Please read http://semver.org/
+ *
+ * You can use the commands
+ *
+ *     gulp patch     # makes v0.1.0 → v0.1.1
+ *     gulp feature   # makes v0.1.1 → v0.2.0
+ *     gulp release   # makes v0.2.1 → v1.0.0
+ *
+ * To bump the version numbers accordingly after you did a patch,
+ * introduced a feature or made a backwards-incompatible release.
+ */
+function increment(importance) {
+    // get all the files to bump version in 
+    return gulp.src(['package.json', 'bower.json', 'composer.json'])
+        // bump the version number in those files 
+        .pipe(bump({ type: importance }))
+
+        // save it back to filesystem 
+        .pipe(gulp.dest('.'))
+
+        // commit the changed version number 
+        .pipe(git.commit('bump packages version'))
+ 
+        // read only one file to get the version number 
+        .pipe(filter('package.json'))
+
+        // **tag it in the repository** 
+        .pipe(tagVersion());
+}
+ 
+gulp.task('patch',   function() { return increment('patch'); });
+gulp.task('feature', function() { return increment('minor'); });
+gulp.task('release', function() { return increment('major'); });
